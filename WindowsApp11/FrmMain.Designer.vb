2ac018369ee0ffb7c07b1b1e6362fab3745921ba
Elbitty <conor.dev@gmail.com>
2017-06-07 12:04:41 +0900

    Names have been changed

---
+++ WindowsApp11/FrmMain.Designer.vb
@@ -0,0 +1,160 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class FrmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form 디자이너에 필요합니다.
+    Private components As System.ComponentModel.IContainer
+
+    '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
+    '수정하려면 Windows Form 디자이너를 사용하십시오.  
+    '코드 편집기에서는 수정하지 마세요.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.pnlShield = New System.Windows.Forms.Panel()
+        Me.lblBack = New System.Windows.Forms.Label()
+        Me.tmrPlay = New System.Windows.Forms.Timer(Me.components)
+        Me.lblTotlSyllableToCurr = New System.Windows.Forms.Label()
+        Me.lblFront = New System.Windows.Forms.Label()
+        Me.lblSpeed = New System.Windows.Forms.Label()
+        Me.lblBeatPerCurSyllable = New System.Windows.Forms.Label()
+        Me.lblCntSpaces = New System.Windows.Forms.Label()
+        Me.btnStart = New System.Windows.Forms.Button()
+        Me.lblCntSyllable = New System.Windows.Forms.Label()
+        Me.pnlShield.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'pnlShield
+        '
+        Me.pnlShield.Controls.Add(Me.lblBack)
+        Me.pnlShield.Location = New System.Drawing.Point(92, 86)
+        Me.pnlShield.Margin = New System.Windows.Forms.Padding(9, 13, 9, 13)
+        Me.pnlShield.Name = "pnlShield"
+        Me.pnlShield.Size = New System.Drawing.Size(10, 315)
+        Me.pnlShield.TabIndex = 4
+        '
+        'lblBack
+        '
+        Me.lblBack.AutoSize = True
+        Me.lblBack.ForeColor = System.Drawing.Color.Blue
+        Me.lblBack.Location = New System.Drawing.Point(3, 0)
+        Me.lblBack.Name = "lblBack"
+        Me.lblBack.Size = New System.Drawing.Size(65, 52)
+        Me.lblBack.TabIndex = 5
+        Me.lblBack.Text = "##"
+        '
+        'tmrPlay
+        '
+        Me.tmrPlay.Interval = 1
+        '
+        'lblTotlSyllableToCurr
+        '
+        Me.lblTotlSyllableToCurr.AutoSize = True
+        Me.lblTotlSyllableToCurr.Location = New System.Drawing.Point(83, 414)
+        Me.lblTotlSyllableToCurr.Name = "lblTotlSyllableToCurr"
+        Me.lblTotlSyllableToCurr.Size = New System.Drawing.Size(34, 52)
+        Me.lblTotlSyllableToCurr.TabIndex = 7
+        Me.lblTotlSyllableToCurr.Text = "."
+        '
+        'lblFront
+        '
+        Me.lblFront.AutoSize = True
+        Me.lblFront.ForeColor = System.Drawing.Color.Red
+        Me.lblFront.Location = New System.Drawing.Point(95, 86)
+        Me.lblFront.Name = "lblFront"
+        Me.lblFront.Size = New System.Drawing.Size(65, 52)
+        Me.lblFront.TabIndex = 8
+        Me.lblFront.Text = "##"
+        '
+        'lblSpeed
+        '
+        Me.lblSpeed.AutoSize = True
+        Me.lblSpeed.Location = New System.Drawing.Point(12, 9)
+        Me.lblSpeed.Name = "lblSpeed"
+        Me.lblSpeed.Size = New System.Drawing.Size(44, 52)
+        Me.lblSpeed.TabIndex = 9
+        Me.lblSpeed.Text = "0"
+        '
+        'lblBeatPerCurSyllable
+        '
+        Me.lblBeatPerCurSyllable.AutoSize = True
+        Me.lblBeatPerCurSyllable.Location = New System.Drawing.Point(432, 9)
+        Me.lblBeatPerCurSyllable.Name = "lblBeatPerCurSyllable"
+        Me.lblBeatPerCurSyllable.Size = New System.Drawing.Size(44, 52)
+        Me.lblBeatPerCurSyllable.TabIndex = 10
+        Me.lblBeatPerCurSyllable.Text = "0"
+        '
+        'lblCntSpaces
+        '
+        Me.lblCntSpaces.AutoSize = True
+        Me.lblCntSpaces.Location = New System.Drawing.Point(555, 9)
+        Me.lblCntSpaces.Name = "lblCntSpaces"
+        Me.lblCntSpaces.Size = New System.Drawing.Size(44, 52)
+        Me.lblCntSpaces.TabIndex = 12
+        Me.lblCntSpaces.Text = "0"
+        '
+        'btnStart
+        '
+        Me.btnStart.Location = New System.Drawing.Point(706, 234)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(269, 151)
+        Me.btnStart.TabIndex = 13
+        Me.btnStart.Text = "START"
+        Me.btnStart.UseVisualStyleBackColor = True
+        '
+        'lblCntSyllable
+        '
+        Me.lblCntSyllable.AutoSize = True
+        Me.lblCntSyllable.Location = New System.Drawing.Point(673, 9)
+        Me.lblCntSyllable.Name = "lblCntSyllable"
+        Me.lblCntSyllable.Size = New System.Drawing.Size(44, 52)
+        Me.lblCntSyllable.TabIndex = 14
+        Me.lblCntSyllable.Text = "0"
+        '
+        'FrmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(22.0!, 52.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(1013, 533)
+        Me.Controls.Add(Me.lblCntSyllable)
+        Me.Controls.Add(Me.btnStart)
+        Me.Controls.Add(Me.lblCntSpaces)
+        Me.Controls.Add(Me.lblBeatPerCurSyllable)
+        Me.Controls.Add(Me.lblSpeed)
+        Me.Controls.Add(Me.lblTotlSyllableToCurr)
+        Me.Controls.Add(Me.pnlShield)
+        Me.Controls.Add(Me.lblFront)
+        Me.Font = New System.Drawing.Font("Noto Sans CJK JP Bold", 26.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(9, 13, 9, 13)
+        Me.Name = "FrmMain"
+        Me.Text = "Form1"
+        Me.pnlShield.ResumeLayout(False)
+        Me.pnlShield.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents pnlShield As Panel
+    Friend WithEvents tmrPlay As Timer
+    Friend WithEvents lblBack As Label
+    Friend WithEvents lblTotlSyllableToCurr As Label
+    Friend WithEvents lblFront As Label
+    Friend WithEvents lblSpeed As Label
+    Friend WithEvents lblBeatPerCurSyllable As Label
+    Friend WithEvents lblCntSpaces As Label
+    Friend WithEvents btnStart As Button
+    Friend WithEvents lblCntSyllable As Label
+End Class
--- WindowsApp11/FrmMain.vb
+++ WindowsApp11/FrmMain.vb
@@ -1,4 +1,4 @@
-﻿Public Class Form1
+﻿Public Class FrmMain
 
     Dim FullLyric As String = "한 번 더 나에게 질풍 같은 용기를 거친 파도에도 굴하지 않게~" '전체 가사
     Dim Lyric As String '스페이스를 제외한 가사
@@ -14,15 +14,15 @@
 
     Dim nextWidth As Integer = -1 '다음번 음절을 포함한 Width 변수
 
-    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles tmrPlay.Tick
 
-        If Panel1.Width > nextWidth Then
+        If pnlShield.Width > nextWidth Then
 
             BeatCursor += 1
             If Mid(FullLyric, BeatCursor, 1) = " " Then '스페이스 스킵용
-                Panel1.Width += (GetCharWidth("$ $", Font) - GetCharWidth("$$", Font)) '더미 텍스트를 스페이스 양쪽에 넣어 스페이스 1개당 차지하는 Width를 구한다.
+                pnlShield.Width += (GetCharWidth("$ $", Font) - GetCharWidth("$$", Font)) '더미 텍스트를 스페이스 양쪽에 넣어 스페이스 1개당 차지하는 Width를 구한다.
 
-                Label7.Text += 1 '디버깅용
+                lblCntSpaces.Text += 1 '디버깅용
             End If
 
             If BeatCursor = 1 Then
@@ -31,34 +31,34 @@
                 Adder = GetCharWidth(Mid(Lyric, 1, BeatCursor + 1), Font) - GetCharWidth(Mid(Lyric, 1, BeatCursor), Font)
             End If
 
-            Label2.Text = Adder '디버깅용
+            lblSpeed.Text = Adder '디버깅용
 
-            nextWidth = Panel1.Width + Adder
+            nextWidth = pnlShield.Width + Adder
 
         Else
 
-            Label3.Text = Mid(Lyric, 1, BeatCursor)
-            Label5.Text = Beat(BeatCursor)
+            lblTotlSyllableToCurr.Text = Mid(Lyric, 1, BeatCursor)
+            lblBeatPerCurSyllable.Text = Beat(BeatCursor)
 
-            Timer1.Interval = (((60 / BPM) * 1000) / Adder * Beat(BeatCursor) * (BeatOverBeat / BeatOverNode))
+            tmrPlay.Interval = (((60 / BPM) * 1000) / Adder * Beat(BeatCursor) * (BeatOverBeat / BeatOverNode))
             'Interval로 음절당 빠르기를 조절한다. Width로 조절하는 것도 가능하나, 윈폼의 한계상 에일리어싱 발생함.
 
-            Label2.Text = Timer1.Interval '디버깅용
+            lblSpeed.Text = tmrPlay.Interval '디버깅용
         End If
 
-        Panel1.Width += 3
+        pnlShield.Width += 3
 
-        Label8.Text = BeatCursor '디버깅용
+        lblCntSyllable.Text = BeatCursor '디버깅용
 
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Label4.Text = FullLyric '뒷쪽 레이블
-        Label1.Text = FullLyric '앞쪽 레이블
+        lblFront.Text = FullLyric '뒷쪽 레이블
+        lblBack.Text = FullLyric '앞쪽 레이블
 
         Lyric = FullLyric.Replace(" ", "") '스페이스는 음절에 해당하지 않으므로 박자 판정을 위하여 제거한다. TODO: 특문 역시 제거가 필요함.
 
-        Timer1.Interval = ((60 / BPM) * 1000)
+        tmrPlay.Interval = ((60 / BPM) * 1000)
     End Sub
 
     Private Function GetCharWidth(A As String, Fontx As Font) As Double '가변폭 폰트에서 한 폰트에 해당하는 Width 값을 구한다.
@@ -68,7 +68,7 @@
 
     End Function
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Timer1.Enabled = True
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+        tmrPlay.Enabled = True
     End Sub
 End Class
